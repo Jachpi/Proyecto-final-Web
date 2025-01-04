f29d86e915d67775432f822697f3bf76f69b3037
Ines <nes.maria.montero@gmail.com>
2025-01-04 13:24:54 +0100

    pequeños cambios css admin y ahora funcionan bien los botones de navegación

--- public/js/admininit.js
+++ public/js/admininit.js
@@ -2,11 +2,11 @@ let pagebutton1 = document.getElementById("pendientes")
 let pagebutton2 = document.getElementById("aprobados")
 
 pagebutton1.addEventListener('click', ()=>{
-    window.location.href = "http://127.0.0.1:5500/public/inicioadmin.html";
+    window.location.href = "/inicioadmin.html";
 
 })
 pagebutton2.addEventListener('click', ()=>{
-    window.location.href = "http://127.0.0.1:5500/public/adminpag2.html";
+    window.location.href = "/adminpag2.html";
     
 })
 
