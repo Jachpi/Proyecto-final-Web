713d07422df405e1c6e1983ff1c7eb6e6593973b
Daniel <hevcuesta@gmail.com>
2025-01-08 16:47:56 +0100

    sql final

--- db.sql
+++ db.sql
@@ -1,48 +1,31 @@
--- PARA USAR CON MYSQL:
+-- DEBIDO A QUE SE HACE USO DE SQLITE NO ES NECESARIO CREAR UNA BASE DE DATOS LOCAL, AUN ASÍ A CONTINUACIÓN SE TIENEN LAS SENTENCIAS QUE CREAN TABLAS
+-- Y RELACIONES ENTRE ELLAS
 
-CREATE TABLE IF NOT EXISTS Usuarios (
-    IDUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    Username VARCHAR(100) UNIQUE NOT NULL,
-    Password VARCHAR(100) NOT NULL,
-    Rol ENUM('Estudiante', 'Profesor', 'Coordinador', 'Dirección', 'Admin') NOT NULL,
-    Email VARCHAR(100) NOT NULL);
+-- Evento definition
 
--- Imagen será una URL o ruta dentro del propio servidor
-CREATE TABLE IF NOT EXISTS Evento (
-    IDEvento INT PRIMARY KEY AUTO_INCREMENT,
-    Nombre VARCHAR(100) NOT NULL,
+CREATE TABLE Evento (
+    IDEvento INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nombre TEXT NOT NULL,
     Descripcion TEXT NOT NULL,
-    Imagen VARCHAR(250) NOT NULL,
+    Imagen TEXT NOT NULL, 
     FechaHora DATETIME NOT NULL,
-    FechaHoraFin DATETIME NOT NULL,
-    IDOwner INT NOT NULL,
-    Categoria ENUM('Evento para estudiantes','Evento para profesores', 'Evento para comunidad universitaria') NOT NULL,
-    Estado ENUM('Pendiente', 'Finalizado', 'Pospuesto') NOT NULL,
-    FOREIGN KEY (IDOwner) REFERENCES Usuarios(id));
+    IDOwner INTEGER NOT NULL,
+    Categoria TEXT 
+        CHECK (Categoria IN ('Evento para estudiantes', 'Evento para profesores', 'Evento para comunidad universitaria'))
+        NOT NULL,
+    Estado TEXT 
+        CHECK (Estado IN ('Pendiente', 'Aprobado', 'Terminado'))
+        NOT NULL DEFAULT 'Pendiente',
+    FechaHoraFin DATETIME NOT NULL DEFAULT '2025-01-01 00:00:00', Ubicacion TEXT,
+    FOREIGN KEY (IDOwner) REFERENCES Usuarios(IDUsuario)
+);
 
+-- Usuarios definition
 
-
--- PARA USAR SQLITE:
-
-
-CREATE TABLE IF NOT EXISTS Usuarios (
+CREATE TABLE Usuarios (
     IDUsuario INTEGER PRIMARY KEY AUTOINCREMENT,
     Username TEXT UNIQUE NOT NULL,
     Password TEXT NOT NULL,
     Rol TEXT CHECK(Rol IN ('Estudiante', 'Profesor', 'Coordinador', 'Dirección', 'Admin')) NOT NULL,
-    isApproved BOOLEAN NOT NULL,
     Email TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Evento (
-    IDEvento INTEGER PRIMARY KEY AUTOINCREMENT,
-    Nombre TEXT NOT NULL,
-    Descripcion TEXT NOT NULL,
-    Imagen TEXT NOT NULL,
-    FechaHora DATETIME NOT NULL,
-    FechaHoraFin DATETIME NOT NULL,
-    IDOwner INTEGER NOT NULL,
-    Categoria TEXT CHECK(Categoria IN ('Evento para estudiantes', 'Evento para profesores', 'Evento para comunidad universitaria')) NOT NULL,
-    Estado TEXT CHECK(Estado IN ('Pendiente', 'Finalizado', 'Pospuesto')) NOT NULL,
-    FOREIGN KEY (IDOwner) REFERENCES Usuarios(IDUsuario)
-);
+, isApproved INTEGER DEFAULT (0));
